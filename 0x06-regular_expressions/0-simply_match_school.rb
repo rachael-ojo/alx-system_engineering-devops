f4a598e659af37547cc9666e0472f31e8974725e
rachael-ojo <adeolaatilade@gmail.com>
2024-02-27 05:00:50 -0800

    Task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,12 +1,9 @@
 #!/usr/bin/env ruby
-
 # Ruby script to accept one argument and check if it contains the word "School"
-
 if ARGV.length != 1
   puts "Usage: ruby script.rb <argument>"
   exit
 end
-
 input = ARGV[0]
 pattern = /School/
 
