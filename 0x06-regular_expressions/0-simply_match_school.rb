d17293ee52906b0d2f14c21c9f38fa0c92a4ca31
rachael-ojo <adeolaatilade@gmail.com>
2024-02-27 12:23:44 -0800

    Task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,23 +1,2 @@
 #!/usr/bin/env ruby
-
-# Ruby script to accept one argument and check if it contains the word "School"
-# Ruby script to generate the output "SchoolSchool$"
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: ruby script.rb <argument>"
-  exit
-end
-
-# Extract the argument
-input = ARGV[0, 1]
-
-# Define the regular expression pattern
-pattern = /\bSchool\b/
-
-# Check if the input matches the pattern
-if input =~ pattern
-  puts 'School'
-  else
-    puts 'school'
-end
+puts ARGV[0].scan(/School/).join
