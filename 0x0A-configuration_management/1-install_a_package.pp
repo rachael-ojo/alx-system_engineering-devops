8c1c23ccd9e21f2cc943a0ff32300b039c4e22e2
rachael-ojo <adeolaatilade@gmail.com>
2024-04-19 14:06:38 -0700

    Updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,11 @@
-package { 'python3-pip':
-  ensure => installed,
+# Ensure Flask package is installed at version 2.1.0 using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+
+# Ensure Werkzeug package is installed at version 2.1.0 using pip3
+package { 'werkzeug':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
