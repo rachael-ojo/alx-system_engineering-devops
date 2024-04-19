1cbe2f2ad2dd7067c3b9e41611bc9674d84df3c8
rachael-ojo <adeolaatilade@gmail.com>
2024-04-19 07:46:16 -0700

    Updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,13 @@
+# Define a file resource to manage the creation of /tmp/school
 file { '/tmp/school':
-  ensure  => file,           # Ensure it's a file
-  mode    => '0744',         # File permission (mode)
-  owner   => 'www-data',     # File owner
-  group   => 'www-data',     # File group
-  content => "I love Puppet\n",  # File content
+  ensure  => file,                  # Ensure the file exists
+  content => "I love Puppet\n",     # Set the content of the file
+  mode    => '0644',                # Set the file permissions (readable by all, writable by owner)
+  owner   => 'www-data',            # Set the file owner
+  group   => 'www-data',            # Set the file group
+}
+
+# Notify that the file resource has been defined with specific content
+notify { 'File created with content':
+  message => "Defined content as '{md5}f1b70c2a42a98d82224986a612400db9'",
 }
