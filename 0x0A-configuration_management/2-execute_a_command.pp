aedbd5ac6f0eb0598bae663e17319488a4a13575
rachael-ojo <adeolaatilade@gmail.com>
2024-04-20 07:51:32 -0700

    Updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
 exec { 'killmenow_process':
-  command  => 'pkill killmenow',
-  onlyif   => 'pgrep killmenow',
-  provider => 'shell',
+  command   => 'pkill killmenow',
+  onlyif    => 'pgrep -x killmenow >/dev/null',
+  provider  => 'shell',
+  logoutput => true,
+  unless    => 'pgrep -x killmenow >/dev/null',
 }
