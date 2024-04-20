43f7cd09d4c78f4cbae1f9039b389ec28d62d020
rachael-ojo <adeolaatilade@gmail.com>
2024-04-20 08:12:35 -0700

    Updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -3,4 +3,12 @@ exec { 'killmenow_process':
   onlyif    => 'pgrep -x killmenow >/dev/null',
   provider  => shell,
   logoutput => true,
+  notify    => Exec['debug_log'],  # Notify a debug logging resource
+}
+
+exec { 'debug_log':
+  command   => 'echo "Executed killmenow_process" >> /tmp/puppet_debug.log',
+  path      => '/bin',  # Specify command path if needed
+  provider  => shell,
+  logoutput => true,
 }
